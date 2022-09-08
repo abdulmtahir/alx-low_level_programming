a8d36efd8f40dd7361a6b5536d136f09ff1f6c86
amtlambu <taheer2005@yahoo.com>
2022-09-08 15:09:12 -0700

    c files

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float c;
-printf("Size of a char: %d byte(s)\n", sizeof(a));
+char a;
+int b;
+long int c;
+long long int d;
+float c;
+printf("Size of a char: %c byte(s)\n", sizeof(a));
 printf("Size of n int: %d byte(s)\n", sizeof(b));
 printf("Size of a long int: %d byte(s)\n", sizeof(c));
 printf("Size of a long long int: %d byte(s)\n", sizeof(d));
-printf("Size of a float: %d byte(s)\n", sizeof(e));
+printf("Size of a float: %f byte(s)\n", sizeof(e));
 return (0);
 }
